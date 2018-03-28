ba2430dd2de289f5d5ea5059b10f73a1b52e27db
adriancro8888 <adriancro8888@gmail.com>
2018-03-28 21:35:10 +0500

    Added UI Changes to Detail View

--- GithubUsers-Redux/Views/UserDetailViewController.swift
+++ GithubUsers-Redux/Views/UserDetailViewController.swift
@@ -21,6 +21,11 @@ class UserDetailViewController: UIViewController {
         }
         struct  User {
             let name: String
+            let userEmail: String
+            let userCompany: String
+            let userLocation: String
+            let userFollowers:Int
+            let userFollowing:Int
             let publicRepos: Int
             let publicGists: Int
         }
@@ -38,6 +43,12 @@ class UserDetailViewController: UIViewController {
     
     @IBOutlet weak var lblPublicRepos: UILabel!
     @IBOutlet weak var lblPublicGist: UILabel!
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet weak var lblUserEmail: UILabel!
+    @IBOutlet weak var lblUserCompany: UILabel!
+    @IBOutlet weak var lblUserLocation: UILabel!
+    @IBOutlet weak var lblUserFollowers: UILabel!
+    @IBOutlet weak var lblUserFollowing: UILabel!
     
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     
@@ -51,8 +62,37 @@ class UserDetailViewController: UIViewController {
         }
         lblPublicGist.isHidden = !loadingIndicator.isHidden
         lblPublicRepos.isHidden = !loadingIndicator.isHidden
+        lblUserName.isHidden = !loadingIndicator.isHidden
+        lblUserEmail.isHidden = !loadingIndicator.isHidden
+        lblUserCompany.isHidden = !loadingIndicator.isHidden
+        lblUserLocation.isHidden = !loadingIndicator.isHidden
+        lblUserFollowers.isHidden = !loadingIndicator.isHidden
+        lblUserFollowing.isHidden = !loadingIndicator.isHidden
         
-        
+        lblUserName.text = result {
+            guard case .user(let user) = props.state else { return "N/A" }
+            return user.name
+        }
+        lblUserEmail.text = result {
+            guard case .user(let user) = props.state else { return "N/A" }
+            return user.userEmail
+        }
+        lblUserCompany.text = result {
+            guard case .user(let user) = props.state else { return "N/A" }
+            return user.userCompany
+        }
+        lblUserLocation.text = result {
+            guard case .user(let user) = props.state else { return "N/A" }
+            return user.userLocation
+        }
+        lblUserFollowers.text = result {
+            guard case .user(let user) = props.state else { return "N/A" }
+            return String(user.userFollowers)
+        }
+        lblUserFollowing.text = result {
+            guard case .user(let user) = props.state else { return "N/A" }
+            return String(user.userFollowing)
+        }
         lblPublicRepos.text = result {
             guard case .user(let user) = props.state else { return "" }
             return String(user.publicRepos)
@@ -87,7 +127,7 @@ extension UserDetailViewController {
             switch state.selectedUser {
             case .none: self?.props = Props.initial
             case .loading(let user): self?.props = Props(name: user.userName, onDestroy: cancelObserving, state: .loading)
-            case .loaded(let userDetail): self?.props = Props(name: userDetail.name!, onDestroy: cancelObserving, state: .user(Props.User(name: userDetail.name!, publicRepos: userDetail.publicRepos, publicGists: userDetail.publicGists)))
+            case .loaded(let userDetail): self?.props = Props(name: userDetail.name ?? "N/A" , onDestroy: cancelObserving, state: .user(Props.User(name: userDetail.name ?? "N/A", userEmail: userDetail.email ?? "N/A", userCompany: userDetail.company ?? "N/A", userLocation: userDetail.location ?? "N/A", userFollowers: userDetail.userFollowers, userFollowing: userDetail.userFollowing, publicRepos: userDetail.publicRepos, publicGists: userDetail.publicGists)))
             }
             
         }
